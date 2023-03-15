64b472da95ca4a0088450cbce20aa10cfe740915
Rafaela Lucia <rafaluciadev@gmail.com>
2023-03-15 14:04:22 -0300

    finalizacao do crud postagem

--- src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
+++ src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
@@ -1,6 +1,9 @@
 package com.generation.blogpessoal.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.generation.blogpessoal.model.Postagem;
@@ -8,6 +11,7 @@ import com.generation.blogpessoal.model.Postagem;
 @Repository
 public interface PostagemRepository extends JpaRepository<Postagem, Long> { //Postagem = tabela, Long = id (tipo)
 
+	public List <Postagem> findAllByTituloContainingIgnoreCase(@Param("titulo") String titulo); //esse titulo está "vindo" da model, entao tem que estar escrito do mesmo jeito que está lá
 	
 	
 }
