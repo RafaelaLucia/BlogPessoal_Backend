222888252fafe9d348889960a019a4a8d08edad3
Rafaela Lucia <rafaluciadev@gmail.com>
2023-03-29 14:30:26 -0300

    Testes Unitários

--- src/main/java/com/generation/blogpessoal/service/UsuarioService.java
+++ src/main/java/com/generation/blogpessoal/service/UsuarioService.java
@@ -76,7 +76,7 @@ public class UsuarioService {
 		
 		String token = usuario + ":" + senha;
 		byte[] tokenBase64 = Base64.encodeBase64(token.getBytes(Charset.forName("US-ASCII")));
-		return "Basic" + new String(tokenBase64);
+		return "Basic " + new String(tokenBase64);
 
 	}
 
